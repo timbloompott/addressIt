e5795b2c3e6fec8705fb5eb4a986d8505da01fe4
Tim Bloompott <timbloompott@gmail.com>
2015-05-13 21:47:54 -0500

    uncommented webbrowser call

--- addressIt.py
+++ addressIt.py
@@ -33,7 +33,7 @@ else:
 	# Get address from clipboard.
 	print('Error')
 
-# webbrowser.open('https://www.google.com/maps/place/' + business + ' ' + city + ' Illinois address')
+webbrowser.open('https://www.google.com/maps/place/' + business + ' ' + city + ' Illinois address')
 # page = curl('https://www.google.com/maps/place/' + business + ' ' + city + ' Illinois address')
 # print(page)
 #	browser.open('https://www.google.com/maps/place/' + address)
